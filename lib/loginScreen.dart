bda04ad695a5ac58a578e0e4a6ab7e4dc32e61fb
Dilip <rexsunar37010@gmail.com>
2022-10-29 12:02:39 +0530

    Second Commit to dilip-branch

--- lib/loginScreen.dart
+++ lib/loginScreen.dart
@@ -6,57 +6,259 @@ class loginScreen extends StatefulWidget {
   _loginScreenState createState() => _loginScreenState();
 }
 
-Widget buildEmail () {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-      Text(
-        'Email',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 16,
-          fontWeight: FontWeight.bold
+class _loginScreenState extends State<loginScreen> {
+
+  bool isRememberMe = false;
+
+  Widget buildEmail () {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const Text(
+          'Email',
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontWeight: FontWeight.bold
+          ),
+        ),
+        SizedBox(height: 10),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: const [
+                BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 6,
+                    offset: Offset(0, 2)
+                )
+              ]
+          ),
+          height: 60,
+          child: const TextField(
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+                color: Colors.black87
+            ),
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(top: 14),
+                prefixIcon: Icon(
+                  Icons.email,
+                  color: Color(0xff5ac18e),
+                ),
+                hintText: 'Email',
+                hintStyle: TextStyle(
+                    color: Colors.black38
+                )
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget buildPassword () {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const Text(
+          'Password',
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontWeight: FontWeight.bold
+          ),
+        ),
+        SizedBox(height: 10),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: const [
+                BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 6,
+                    offset: Offset(0, 2)
+                )
+              ]
+          ),
+          height: 60,
+          child: const TextField(
+            obscureText: true,
+            style: TextStyle(
+                color: Colors.black87
+            ),
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(top: 14),
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: Color(0xff5ac18e),
+                ),
+                hintText: 'Password',
+                hintStyle: TextStyle(
+                    color: Colors.black38
+                )
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget buildConfirmPassword () {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const Text(
+          'Confirm Password',
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontWeight: FontWeight.bold
+          ),
+        ),
+        SizedBox(height: 10),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: const [
+                BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 6,
+                    offset: Offset(0, 2)
+                )
+              ]
+          ),
+          height: 60,
+          child: const TextField(
+            obscureText: true,
+            style: TextStyle(
+                color: Colors.black87
+            ),
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(top: 14),
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: Color(0xff5ac18e),
+                ),
+                hintText: 'Confirm Password',
+                hintStyle: TextStyle(
+                    color: Colors.black38
+                )
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  // Widget buildForgotPasswordBtn () {
+  //   return Container(
+  //     alignment: Alignment.centerRight,
+  //     child: FlatButton (
+  //       onPressed: () => print("Forgot Password Pressed"),
+  //       padding: EdgeInsets.only(right: 0),
+  //       child: Text (
+  //         'Forgot Password?',
+  //         style: TextStyle(
+  //             color: Colors.white,
+  //             fontWeight: FontWeight.bold
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
+
+  // Widget buildRememberCb() {
+  //   return Container(
+  //     height: 20,
+  //     child: Row(
+  //       children: <Widget>[
+  //         Theme(
+  //           data: ThemeData(unselectedWidgetColor: Colors.white),
+  //           child: Checkbox(
+  //             value: isRememberMe,
+  //             checkColor: Colors.green,
+  //             activeColor: Colors.white,
+  //             onChanged: (value) {
+  //               setState(() {
+  //                 isRememberMe = value!;
+  //               });
+  //             },
+  //           ),
+  //         ),
+  //         const Text (
+  //           'Remember me',
+  //           style: TextStyle (
+  //             color: Colors.white,
+  //             fontWeight: FontWeight.bold
+  //           ),
+  //         )
+  //       ],
+  //     ),
+  //   );
+  // }
+
+  Widget buildSignUpBtn () {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25),
+      width: double.infinity,
+      child: RaisedButton(
+        elevation: 5,
+        onPressed: () => print('Signup Pressed!'),
+        padding: const EdgeInsets.all(15),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15)
+        ),
+        color: Colors.white,
+        child: const Text (
+          'SIGN UP',
+          style: TextStyle(
+            color: Color(0xff5ac18e),
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
-      SizedBox(height: 10),
-      Container(
-        alignment: Alignment.centerLeft,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 6,
-              offset: Offset(0, 2)
+    );
+  }
+
+  Widget buildLoginBtn () {
+    return GestureDetector(
+      onTap: () => print("Login Pressed!"),
+      child: RichText(
+        text: const TextSpan(
+          children: [
+            TextSpan(
+              text: 'Already have an account? ',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.w500
+              )
+            ),
+            TextSpan(
+              text: 'Login',
+              style: TextStyle (
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold
+              )
             )
           ]
         ),
-        height: 60,
-        child: TextField(
-          keyboardType: TextInputType.emailAddress,
-          style: TextStyle(
-            color: Colors.black87
-          ),
-          decoration: InputDecoration(
-            border: InputBorder.none,
-            contentPadding: EdgeInsets.only(top: 14),
-            prefixIcon: Icon(
-              Icons.email,
-              color: Color(0xff5ac18e),
-            ),
-            hintText: 'Email',
-            hintStyle: TextStyle(
-              color: Colors.black87
-            )
-          ),
-        ),
-      )
-    ],
-  );
-}
+      ),
+    );
+  }
 
-class _loginScreenState extends State<loginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,7 +270,7 @@ class _loginScreenState extends State<loginScreen> {
               Container(
                 height: double.infinity,
                 width: double.infinity,
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                   gradient: LinearGradient(
                     begin: Alignment.topCenter,
                     end: Alignment.bottomCenter,
@@ -80,20 +282,36 @@ class _loginScreenState extends State<loginScreen> {
                     ]
                   )
                 ),
-                child: Column (
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      'Sign Up',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold
+                child: SingleChildScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 25,
+                    vertical: 120
+                  ),
+                  child: Column (
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const Text(
+                        'Sign Up',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 50),
-                    buildEmail(),
-                  ],
+                      const SizedBox(height: 50),
+                      buildEmail(),
+                      const SizedBox(height: 20),
+                      buildPassword(),
+                      const SizedBox(height: 20),
+                      buildConfirmPassword(),
+                      const SizedBox(height: 30),
+                      // buildForgotPasswordBtn(),
+                      // buildRememberCb(),
+                      buildSignUpBtn(),
+                      buildLoginBtn(),
+                    ],
+                  ),
                 ),
               )
             ],
