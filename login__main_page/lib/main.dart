1cdfb499a40f2d1e068ad60bcf2fd4cbb28ef40a
amankr28 <115891417+amankr28@users.noreply.github.com>
2022-11-12 21:49:57 +0530

    Update main.dart

--- login__main_page/lib/main.dart
+++ login__main_page/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sign up',
+      title: 'Login',
       debugShowCheckedModeBanner: false,
       home: loginScreen(),
     );
